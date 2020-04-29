64d745b03f313108dd8280f4edb9db2e7e90e0c7
Vivek Sekhar <vivek@viveksekhar.ca>
2020-04-28 21:40:43 -0400

    Initial implementation

---
+++ chronytime.go
@@ -0,0 +1,283 @@
+package chronytime
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"net"
+	"time"
+	"unsafe"
+)
+
+// UNIX domain socket might be available if we are running as chrony user or
+// root, but regular users will connect via UDP
+// const defaultCommandSocket = "/var/run/chrony/chronyd.sock"
+const defaultCandMPort = 323
+
+var networkOrder = binary.BigEndian
+
+// Packet types (request.pktType and response.PktType)
+const (
+	pktTypeCmdRequest = 1
+	pktTypeCmdReply   = 2
+)
+
+// Commands (request.command and response.Command)
+const (
+	cmdTracking = 33 // also used for waitSync
+)
+
+// Replies (response.Reply)
+const (
+	rpyNull     = 1
+	rpyTracking = 5
+)
+
+// Statuses
+const (
+	sttSuccess = 0
+)
+
+type timeSpec struct {
+	SecHigh uint32
+	SecLow  uint32
+	Nsec    uint32
+}
+
+func (t *timeSpec) Time() time.Time {
+	return time.Unix((int64(t.SecHigh)<<32)+int64(t.SecLow), int64(t.Nsec))
+}
+
+const (
+	ipAddrFamilyUnspec = 0
+	ipAddrFamilyINET4  = 1
+	ipAddrFamilyINET6  = 2
+	ipAddrFamilyID     = 3
+)
+
+type ipAddr struct {
+	Addr    [16]byte // ipv4 - 4 bytes, ipv6 - 16 bytes, id - 4 bytes
+	Family  uint16
+	Padding uint16
+}
+
+/* 32-bit floating-point format consisting of 7-bit signed exponent
+   and 25-bit signed coefficient without hidden bit.
+   The result is calculated as: 2^(exp - 25) * coef */
+type cfloat struct {
+	F int32
+}
+
+const (
+	cfloatExpBits  = 7
+	cfloatExpMin   = (-(1 << (cfloatExpBits - 1)))
+	cfloatExpMax   = (-cfloatExpMin - 1)
+	cfloatCoefBits = (unsafe.Sizeof(int32(0))*8 - cfloatExpBits)
+	cfloatCoefMin  = (-(1 << (cfloatCoefBits - 1)))
+	cfloatCoefMax  = (-cfloatCoefMin - 1)
+)
+
+func (f *cfloat) value() float64 {
+	var exp, coef int32
+	x := uint32(f.F)
+	exp = int32(x >> cfloatCoefBits)
+	if exp >= 1<<(cfloatExpBits-1) {
+		exp -= 1 << cfloatExpBits
+	}
+	exp -= int32(cfloatCoefBits)
+
+	coef = int32(x % (1 << cfloatCoefBits))
+	if coef >= 1<<(cfloatCoefBits-1) {
+		coef -= 1 << cfloatCoefBits
+	}
+
+	return float64(coef) * math.Pow(2.0, float64(exp))
+}
+
+type request struct {
+	version  uint8
+	pktType  uint8
+	res1     uint8
+	res2     uint8
+	command  uint16
+	attempt  uint16
+	sequence [4]byte
+	pad1     [8]byte
+
+	// union of request structs, largest of which is REQ_NTP_Source (93 bytes)
+	pad2 [93]byte
+
+	// actual padding to prevent data amplification attacks
+	padding [396]byte
+}
+
+type trackingResponse struct {
+	RefID              uint32
+	Addr               ipAddr
+	Stratum            uint16
+	LeapStatus         uint16
+	RefTime            timeSpec
+	CurrentCorrection  cfloat
+	LastOffset         cfloat
+	RmsOffset          cfloat
+	FreqPPM            cfloat
+	ResidFreqPPM       cfloat
+	SkewPPM            cfloat
+	RootDelay          cfloat
+	RootDispersion     cfloat
+	LastUpdateInterval cfloat
+	EOR                int32
+}
+
+func uncertainty(r trackingResponse) time.Duration {
+	// https://chrony.tuxfamily.org/doc/3.5/chronyc.html
+	correction := r.CurrentCorrection.value()
+	rootDelay := r.RootDelay.value()
+	rootDispersion := r.RootDispersion.value()
+	s := math.Abs(correction) + rootDispersion + (0.5 * rootDelay)
+	return time.Duration(s) * time.Second
+}
+
+type response struct {
+	Version  uint8
+	PktType  uint8
+	Res1     uint8
+	Res2     uint8
+	Command  uint16
+	Reply    uint16
+	Status   uint16
+	Pad1     uint16
+	Pad2     uint16
+	Pad3     uint16
+	Sequence uint32
+	Pad4     uint32
+	Pad5     uint32
+
+	// in C: union of lots of response types, we just use trackingResponse
+	Tracking trackingResponse
+}
+
+// Client is a chronytime client.
+type Client struct {
+	addr *net.UDPAddr
+	conn *net.UDPConn
+}
+
+// NewClient creates a new chronytime client and attempts to connect to a local
+// chronyd instance.
+func NewClient() (*Client, error) {
+	s, err := net.ResolveUDPAddr("udp4", fmt.Sprintf("127.0.0.1:%d", defaultCandMPort))
+	conn, err := net.DialUDP("udp4", nil, s)
+	if err != nil {
+		return nil, err
+	}
+	c := &Client{addr: s, conn: conn}
+
+	if err := c.waitSync(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func sameUDPAddr(a1, a2 net.UDPAddr) bool {
+	if a1.IP.Equal(a2.IP) &&
+		a1.Port == a2.Port &&
+		a1.Zone == a2.Zone {
+		return true
+	}
+	return false
+}
+
+func (c *Client) waitSync() error {
+	attempts := 0
+	maxAttempts := 3
+	for {
+		attempts++
+		if attempts > maxAttempts {
+			return fmt.Errorf("max attempts exceeded waiting for sync")
+		}
+		r, err := c.trackingRequest()
+		if err != nil {
+			return err
+		}
+		// no sync
+		if r.Tracking.Addr.Family == ipAddrFamilyUnspec {
+			continue
+		}
+		// local sync
+		if r.Tracking.RefID == 0 || r.Tracking.RefID == 0x7f7f0101 /* LOCAL refid */ {
+			continue
+		}
+		// too much uncertainty
+		if uncertainty(r.Tracking) > (20 * time.Millisecond) {
+			continue
+		}
+		// bad packet or parse
+		if r.Tracking.EOR != 0 {
+			continue
+		}
+		break
+	}
+	return nil
+}
+
+func (c *Client) trackingRequest() (*response, error) {
+	r := request{
+		version: 6,
+		pktType: pktTypeCmdRequest,
+		command: cmdTracking,
+		attempt: 0,
+	}
+	rand.Read(r.sequence[:])
+	if err := binary.Write(c.conn, networkOrder, r); err != nil {
+		return nil, err
+	}
+	buffer := make([]byte, 1024)
+	for {
+		c.conn.SetDeadline(time.Now().Add(1 * time.Second))
+		n, addr, err := c.conn.ReadFromUDP(buffer)
+		if n == 0 {
+			return nil, fmt.Errorf("empty read")
+		}
+		if !sameUDPAddr(*addr, *c.addr) {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+		break
+	}
+	reader := bytes.NewReader(buffer)
+	rep := new(response)
+	if err := binary.Read(reader, networkOrder, rep); err != nil {
+		return nil, err
+	}
+	return rep, nil
+}
+
+// Close closes the client.
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
+
+// WaitUntilAfter blocks until chronytime is sure the current time is after t.
+// If an error occurs while waiting, the operation is aborted and the error is returned.
+func (c *Client) WaitUntilAfter(t time.Time) error {
+	for {
+		r, err := c.trackingRequest()
+		if err != nil {
+			return err
+		}
+		now := time.Now()
+		eps := uncertainty(r.Tracking)
+		earliest := now.Add(-eps)
+		if earliest.After(t) {
+			break
+		}
+		time.Sleep(t.Sub(earliest))
+	}
+	return nil
+}
---
+++ chronytime_test.go
@@ -0,0 +1,117 @@
+package chronytime
+
+import (
+	"bytes"
+	"encoding/binary"
+	"reflect"
+	"testing"
+	"time"
+
+	"gonum.org/v1/gonum/floats"
+)
+
+func TestClient(t *testing.T) {
+	c, err := NewClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+}
+
+type cfloatCase struct {
+	f    int32
+	want float64
+	ulp  uint
+}
+
+func TestCFloat(t *testing.T) {
+	cases := []cfloatCase{
+		{f: -320148152, want: 0.000448087, ulp: 1e10},
+		{f: -349885382, want: -0.000208690, ulp: 1e10},
+		{f: -356455327, want: 0.000183986, ulp: 1e10},
+		{f: 182955620, want: 14.480, ulp: 1e11},
+		{f: -213802485, want: -0.003, ulp: 1e15},
+		{f: -87438093, want: 0.049, ulp: 1e14},
+		{f: -154422419, want: 0.012432915, ulp: 1e8},
+		{f: -254273351, want: 0.001648686, ulp: 1e8},
+		{f: 411118241, want: 1033.3, ulp: 1e12},
+	}
+	for _, c := range cases {
+		cf := cfloat{F: c.f}
+		if !floats.EqualWithinULP(c.want, cf.value(), c.ulp) {
+			t.Errorf("Want: %f, got %f", c.want, cf.value())
+		}
+	}
+}
+
+func TestResponseParse(t *testing.T) {
+	/*
+		$ strace -f -e trace=network -x -s 10000 chronyc tracking
+		< strace binary data --> testVec >
+		Reference ID    : CE6C0084 (ntp2.torix.ca)
+		Stratum         : 2
+		Ref time (UTC)  : Tue Apr 28 23:01:19 2020
+		System time     : 0.000448087 seconds slow of NTP time
+		Last offset     : -0.000208690 seconds
+		RMS offset      : 0.000183986 seconds
+		Frequency       : 14.480 ppm fast
+		Residual freq   : -0.003 ppm
+		Skew            : 0.049 ppm
+		Root delay      : 0.012432915 seconds
+		Root dispersion : 0.001648686 seconds
+		Update interval : 1033.3 seconds
+		Leap status     : Normal
+	*/
+	testVec := []byte("\x06\x02\x00\x00\x00\x21\x00\x05\x00\x00\x00\x00\x00\x00\x00\x00\xa2\x15\x69\x52\x00\x00\x00\x00\x00\x00\x00\x00\xce\x6c\x00\x84\xce\x6c\x00\x84\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x5e\xa8\xb5\xbf\x1c\x98\xf9\xc4\xec\xea\xed\x48\xeb\x25\x2c\x3a\xea\xc0\xec\x61\x0a\xe7\xae\x64\xf3\x41\xa2\x0b\xfa\xc9\xcc\xf3\xf6\xcb\xb3\x6d\xf0\xd8\x18\xb9\x18\x81\x2a\xa1")
+	// the C code silently ignores fields at the end, but binary.Read is too smart for that
+	// so add enough padding to prevent an "unexpected EOF" error
+	padding := 4
+	testVec = append(testVec, make([]byte, padding)...)
+	testRep := response{
+		Version:  6,
+		PktType:  pktTypeCmdReply,
+		Command:  cmdTracking,
+		Reply:    rpyTracking,
+		Status:   sttSuccess,
+		Sequence: 2719312210,
+		Tracking: trackingResponse{
+			RefID: 3463184516,
+			Addr: ipAddr{
+				Addr:   [16]byte{206, 108, 0, 132, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				Family: 1,
+			},
+			Stratum:    2,
+			LeapStatus: 0,
+			RefTime: timeSpec{
+				SecHigh: 0,
+				SecLow:  1588114879,
+				Nsec:    479787460,
+			},
+			CurrentCorrection:  cfloat{F: -320148152},
+			LastOffset:         cfloat{F: -349885382},
+			RmsOffset:          cfloat{F: -356455327},
+			FreqPPM:            cfloat{F: 182955620},
+			ResidFreqPPM:       cfloat{F: -213802485},
+			SkewPPM:            cfloat{F: -87438093},
+			RootDelay:          cfloat{F: -154422419},
+			RootDispersion:     cfloat{F: -254273351},
+			LastUpdateInterval: cfloat{F: 411118241},
+			EOR:                0,
+		},
+	}
+	//reader := &zeroReader{r: bytes.NewReader(testVec)}
+	reader := bytes.NewReader(testVec)
+	rep := new(response)
+	if err := binary.Read(reader, networkOrder, rep); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(&testRep, rep) {
+		t.Logf("Wanted: %+v", testRep)
+		t.Errorf("Got: %+v", rep)
+	}
+	rt := rep.Tracking.RefTime.Time().UTC().Format(time.ANSIC)
+	wantrts := "Tue Apr 28 23:01:19 2020"
+	if rt != wantrts {
+		t.Errorf("Wanted: %s, got %s", wantrts, rt)
+	}
+}
